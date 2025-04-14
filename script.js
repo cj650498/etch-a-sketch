91e52e53f750ff0567cd2a75d27161dbcc94ea1f
Caleb Jones <cj650498@icloud.com>
2025-04-14 17:35:28 -0400

    Add event listener to generate square grid

--- script.js
+++ script.js
@@ -29,3 +29,26 @@ function createSquare(size) {
     square.classList.add("square");
     return square;
 }
+
+const setSquaresBtn = document.querySelector(".set-squares-btn");
+
+setSquaresBtn.addEventListener("click", () => {
+    generateSquares(getValidInput());
+});
+
+function generateSquares(squaresPerSide) {
+
+    // Get a reference to container and clear it
+    const container = document.querySelector(".container");
+    clearContainer(container);
+
+    // Get size and number of squares
+    const squareSize = getSquareSize(container, squaresPerSide);
+    const numberOfSquares = squaresPerSide * squaresPerSide;
+
+    // Create and append the squares to the container
+    for (let i = 0; i < numberOfSquares; ++i) {
+        const square = createSquare(squareSize);
+        container.appendChild(square);
+    }
+}
