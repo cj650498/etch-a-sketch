22c68dfb12b2637f4fab62c1507cb02ffb06cc17
Caleb Jones <cj650498@icloud.com>
2025-04-16 15:54:30 -0400

    Refactor grid generation

--- script.js
+++ script.js
@@ -1,91 +1,76 @@
 const basicColorKeywords = [
-    "black", "white", "red", "green", "blue",
-    "yellow", "cyan", "magenta", "gray", "orange",
+    "black", "red", "green",
+    "blue", "yellow", "cyan",
+    "magenta", "gray", "orange",
     "brown", "purple", "pink"
-  ];
+];
 
-const MAX_SQUARES = 100;
-const MIN_SQUAERES = 1;
-const container = document.querySelector(".container");
-const clearBtn = document.querySelector(".clear-btn");
-const setSquaresBtn = document.querySelector(".set-squares-btn");
+const INITIAL_GRID_SIZE = 20;
+const MAX_GRID_SIZE = 100;
+const MIN_GRID_SIZE = 1;
 
-container.addEventListener("mouseover", (event) => {
-    const currentSquare = event.target;
-    const randomBasicColor = randomColor();
-    colorSquare(currentSquare, randomBasicColor);
+const setGridSizeBtn = document.querySelector(".set-grid-size-btn");
+const resetColorsBtn = document.querySelector(".reset-colors-btn");
+
+window.onload = () => {
+    setGridSize(INITIAL_GRID_SIZE);
+};
+
+// Generate squares (user given input)
+setGridSizeBtn.addEventListener("click", () => {
+    removeAllContainers();
+    const squaresPerSide = getValidInput();
+    setGridSize(squaresPerSide);
 });
 
-setSquaresBtn.addEventListener("click", () => {
-    generateSquares(getValidInput());
+// Set background to white for all squares
+resetColorsBtn.addEventListener("click", () => {
+    document.querySelectorAll(".square").forEach(square => setBackground(square, "white"));
 });
 
-clearBtn.addEventListener("click", () => {
-    clearColor(container);
+// Set background to random color for square that is hovered over
+document.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("square")) {
+        setBackground(event.target, generateRandomColor());
+    }
 });
 
 function getValidInput() {
     let input;
-
     do {
         input = prompt("Enter how many squares per side (max 100):");
     } while (
         input === null ||
         input.trim() === "" ||
         isNaN(input) ||
-        input < MIN_SQUAERES || input > MAX_SQUARES
+        input < MIN_GRID_SIZE || input > MAX_GRID_SIZE
     );
-
     return Number(input);
 }
 
-function clearContainer(container) {
-    if (container.hasChildNodes()) {
-        container.replaceChildren();
-    }
-}
-
-function getSquareSize(container, squaresPerSide) {
-    return container.offsetWidth / squaresPerSide;
-}
-
-function createSquare(size) {
-    const square = document.createElement("div");
-    square.style.setProperty("--size", `${size}px`);
-    square.style.setProperty("--bgColor", "white");
-    square.classList.add("square");
-    return square;
-}
-
-function clearColor(container) {
-    for (let i = 0; i < container.children.length; ++i) {
-        const square = container.children[i];
-        colorSquare(square, "white");
-    }
-}
-
-function generateSquares(squaresPerSide) {
-
-    // Get a reference to container and clear it
-    const container = document.querySelector(".container");
-    clearContainer(container);
-
-    // Get size and number of squares
-    const squareSize = getSquareSize(container, squaresPerSide);
-    const numberOfSquares = squaresPerSide * squaresPerSide;
-
-    // Create and append the squares to the container
-    for (let i = 0; i < numberOfSquares; ++i) {
-        const square = createSquare(squareSize);
-        container.appendChild(square);
-    }
-}
-
-function colorSquare(square, color) {
+function setBackground(square, color) {
     square.style.setProperty("--bgColor", `${color}`);
 }
 
-function randomColor() {
+function generateRandomColor() {
     const randomIndex = Math.floor(Math.random() * basicColorKeywords.length);
     return basicColorKeywords[randomIndex];
+}
+
+function removeAllContainers() {
+    document.querySelectorAll(".container").forEach(container => container.remove());
+}
+
+function setGridSize(squaresPerSide) {
+    for (let i = 0; i < squaresPerSide; ++i) {
+        const container = document.createElement("div");
+        container.classList.add("container");
+        document.body.appendChild(container);
+
+        for (let j = 0; j < squaresPerSide; ++j) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            container.appendChild(square);
+        }
+    }
 }
